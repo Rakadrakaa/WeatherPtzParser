d3fa83e358f8377bb4d6e7a1a9df4f836f1b6a69
Denis Rodionov <rodionof.denis@yandex.ru>
2022-07-24 11:58:29 +0300

    Дополнен функционал

---
+++ parser.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://pogoda-karelia.ru/'
+response = requests.get(url)
+
+bs = BeautifulSoup(response.text, 'lxml')
+
+title_dis = bs.find('h4', class_="title")  # Получение названия района
+temp = bs.find('span', class_="t-value")  # Получение температуры
+date = bs.find('h5', class_="date")  # Получение даты
+time = bs.find('h5', class_="time")  # Получение времени
+
+print(
+    f'Район {title_dis.text}',
+    f'Текущая температура {temp.text} градусов по Цельсию',
+    f'Дата {date.text} г.',
+    f'Время {time.text}', sep='\n')
