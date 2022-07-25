3445817199d29a8b9ed3fd9356e51d6ffd0ffd8b
Denis Rodionov <rodionof.denis@yandex.ru>
2022-07-25 23:15:21 +0300

    modified:   parser.py modified:   weather.csv

--- parser.py
+++ parser.py
@@ -1,35 +1,50 @@
-# TODO: Прикрутить телеграмм бота
 from bs4 import BeautifulSoup
 import requests
 import csv
+import telebot
 
+bot = telebot.TeleBot(
+    '5449903974:AAH6E3-sgSSDVlw49eUA3W0zhbYUkiTGiXQ')  # API бота
 
-url = 'http://pogoda-karelia.ru/'
+url = 'http://pogoda-karelia.ru/'  # Ссылка на сайт
 response = requests.get(url)
 
 bs = BeautifulSoup(response.text, 'lxml')
 
-title_dis = bs.find('h4', class_='title')  # Получение названия района
-temp = bs.find('span', class_="t-value")  # Получение температуры
-date = bs.find('h5', class_="date")  # Получение даты
-time = bs.find('h5', class_="time")  # Получение времени
+title_dis = bs.find('h4', class_='title').text  # Получение названия района
+temp = bs.find('span', class_="t-value").text  # Получение температуры
+date = bs.find('h5', class_="date").text  # Получение даты
+time = bs.find('h5', class_="time").text  # Получение времени
 
-title_dis = title_dis.text
-temp = temp.text
-date = date.text
-time = time.text
+tg_title = 'Район: ' + title_dis
+tg_temp = 'Температура: ' + temp
+tg_date = 'Дата: ' + date
+tg_time = 'Время: ' + time
 
-print(
-    f'Район {title_dis}',
-    f'Текущая температура {temp} градусов по Цельсию',
-    f'Дата {date} г.',
-    f'Время {time}', sep='\n')
 
-with open("weather.csv", mode="a", encoding='utf-8') as w_file:
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Привет, показать погоду?')
+
+
+@bot.message_handler(content_types=['text'])
+def send_weather(message):
+    bot.send_message(message.chat.id, "Погода на сейчас: ")
+    bot.send_message(message.chat.id, tg_title)
+    bot.send_message(message.chat.id, tg_temp)
+    bot.send_message(message.chat.id, tg_date)
+    bot.send_message(message.chat.id, tg_time)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+
+
+with open("weather.csv", mode="a", encoding='utf-8') as w_file:  # Запись рез. парсинга в csv файл
     headers = ["District", "Temperatue", "Date", "Time"]
     file_writer = csv.DictWriter(w_file, delimiter=",",
                                  lineterminator="\r", fieldnames=headers)
     file_writer.writerow(
         {'District': title_dis, 'Temperatue': temp, 'Date': date, 'Time': time})
 
-file_writer = csv.writer(w_file, delimiter="\t")
+    file_writer = csv.writer(w_file, delimiter="\t")
